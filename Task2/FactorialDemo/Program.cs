63864b3fbdf81b0516b10d3aaff45c3aafb94e1c
PavelGamezo <pashasblv@gmail.com>
2022-07-15 13:03:40 +0300

    Completed homework2

---
+++ Task2/FactorialDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace FactorialDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 0;
+            Console.WriteLine("Enter your number: ");
+            number = int.Parse(Console.ReadLine());
+            Console.WriteLine("{0}! = {1}", number, Factorial(number));
+        }
+
+        static int Factorial(int number)
+        {
+            var factorial = 1;
+            for (int i = 2; i <= number; i++)
+            {
+                factorial *= i;
+            }
+            return factorial;
+        }
+    }
+}
