754a81e7b6582304e4ebfea75dede6f542e61c95
PavelGamezo <pashasblv@gmail.com>
2022-07-22 17:55:48 +0300

    My homework 4 completed

---
+++ Homework 4/Homework 4.1/Homework 4.1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Homework_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sizeOfArray, sum = 0;
+
+            Console.Write("Enter your number N: ");
+            sizeOfArray = int.Parse(Console.ReadLine());
+
+            int[] array = new int[sizeOfArray];
+            Console.WriteLine("Enter your array elements: ");
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+            }
+            ShowArray(array);
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    sum += array[i];
+                }
+            }
+            Console.WriteLine($"Sum of array elements with odd indices: {sum}");
+
+            int minElement = array[0];
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (minElement > array[i])
+                {
+                    minElement = array[i];
+                }
+            }
+            Console.WriteLine($"Your minimal element: {minElement}");
+        }
+
+        static void ShowArray(int[] array)
+        {
+            Console.WriteLine("Your array: ");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write("|" + array[i]);
+            }
+            Console.WriteLine("|");
+        }
+    }
+}
---
+++ Homework 4/Homework 4.2/Homework 4.2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Homework_4._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sizeOfArray;
+            Console.Write("Enter your array size: ");
+            sizeOfArray = int.Parse(Console.ReadLine());
+            int[] array = new int[sizeOfArray];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = i;
+            }
+
+            Console.WriteLine("Your array: ");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write("|" + array[i]);
+            }
+            Console.WriteLine("|");
+
+            Console.WriteLine("Your stack: ");
+            Stack<int> stack = new Stack<int>(array);
+            foreach (var item in stack)
+            {
+                Console.Write("|" + item);
+            }
+            Console.WriteLine("|");
+        }
+    }
+}
